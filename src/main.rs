d6a516e10a219cd25159681bb891a0e668268ae5
EchoAlice <echoalice0@gmail.com>
2023-05-18 19:07:01 -0600

    Configure dummy nodes

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,40 @@
 #![allow(unused)]
 
-use sha2::{Digest, Sha256};
-use crate::{kbucket::KbucketTable,
-            helper::Node,
-};
+use std::net::Ipv4Addr;
 
-pub mod kbucket;
+use crate::{helper::Node, kbucket::KbucketTable};
+use sha2::{Digest, Sha256};
+
 pub mod helper;
+pub mod kbucket;
 
 fn main() {
+    // Should these nodes have different IP addresses?  Stupid question- but I'm asking anyways   :P
+    let listen_addr = String::from("127.0.0.1").parse::<Ipv4Addr>().unwrap();
+    let port_start = (9000 as u16);
+
     // Routing Table Logic
     let local_node = Node {
-        ip_address: "random",
-        udp_port: "words",
+        ip_address: listen_addr,
+        udp_port: port_start,
         node_id: [0 as u8; 32],
     };
     let first_node_to_add = Node {
-        ip_address: "first",
-        udp_port: "node",
+        ip_address: listen_addr,
+        udp_port: port_start + 1,
         node_id: [1 as u8; 32],
     };
     let second_node_to_add = Node {
-        ip_address: "second",
-        udp_port: "node",
+        ip_address: listen_addr,
+        udp_port: port_start + 2,
         node_id: [2 as u8; 32],
     };
     let third_node_to_add = Node {
-        ip_address: "second",
-        udp_port: "node",
+        ip_address: listen_addr,
+        udp_port: port_start + 3,
         node_id: [3 as u8; 32],
     };
-    
+
     let mut local_nodes_rt = KbucketTable::new(local_node.node_id);
 
     // TODO:  Implement real deal tests!
@@ -50,9 +54,7 @@ fn main() {
     println!("\n");
     let result = local_nodes_rt.find_node(third_node_to_add);
     println!("\n");
-    
+
     // Verify Table at a glance
     println!("Node's routing table: {:?}", local_nodes_rt.buckets);
-
-
 }
