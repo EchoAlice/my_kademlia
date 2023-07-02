607999ba6e43e6328369e87b9676830377e219a7
EchoAlice <echoalice0@gmail.com>
2023-07-02 13:51:11 -0600

    Implement conversion logic for FindNode.  Message -> Bytes

--- src/message.rs
+++ src/message.rs
@@ -1,46 +1,47 @@
 use crate::helper::Identifier;
 use crate::kbucket::TableRecord;
 use std::collections::HashMap;
+use std::convert::From;
 
-#[derive(Clone, Debug, PartialEq)]
-pub enum Message {
-    Ping([u8; 1024]),     // b"01"
-    Pong([u8; 1024]),     // b"02"
-    FindNode([u8; 1024]), // b"03"
+// TODO: Alias u8 = session
+
+#[derive(Debug)]
+pub struct Message {
+    pub session: u8,
+    pub body: MessageBody,
 }
 
-pub fn create_message(
-    mtype: &[u8; 2],
-    local_id: &Identifier,
-    session_number: &u8,
-    peers: Option<&HashMap<[u8; 32], TableRecord>>,
-) -> [u8; 1024] {
-    let mut message = [0u8; 1024];
-    message[0..2].copy_from_slice(mtype);
-    message[2] = *session_number;
-    message[3..35].copy_from_slice(local_id);
-
-    // TODO: Use Some() None logic here
-
-    if let Some(peers) = peers {
-    } else {
+// TODO: Properly implement From trait.
+impl From<Message> for Vec<u8> {
+    fn from(item: Message) -> Self {
+        let mut out = Vec::new();
+        match item.body {
+            MessageBody::Ping(body) => {
+                unimplemented!()
+            }
+            MessageBody::Pong(body) => {
+                unimplemented!()
+            }
+            MessageBody::FindNode(id) => {
+                for byte in b"03" {
+                    out.push(*byte);
+                }
+                out.push(item.session);
+                for byte in id {
+                    out.push(byte);
+                }
+            }
+        };
+        out
     }
+}
 
-    if &message[0..2] == b"03" {
-        println!("TODO: Place peers in message: {:?}", peers);
-        /*
-        ChatGPT says...
-
-        for (i, item) in data.iter().enumerate() {
-            let start = 35 + i * Identifier.len();
-            let end = start + item.len();
-            message[start..end].copy_from_slice(item);
-        }
-         */
-    }
-
-    message
+#[derive(Clone, Debug, PartialEq)]
+pub enum MessageBody {
+    Ping(u8),             // b"01"
+    Pong(u8),             // b"02"
+    FindNode(Identifier), // b"03"
 }
 
 // TODO: Consolidate logic from ping and find_node.  This may need to be placed
-pub fn request_message() /*-> u8*/ {}
+// pub fn request_message() /*-> u8*/ {}
