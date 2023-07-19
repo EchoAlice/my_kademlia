e97155c6e3421b49ccc18ea990fd879637a11bc8
EchoAlice <echoalice0@gmail.com>
2023-07-19 13:35:22 -0600

    Create get_closest_node()

--- src/kbucket.rs
+++ src/kbucket.rs
@@ -53,18 +53,36 @@ impl KbucketTable {
         bucket.map.get(id)
     }
 
+    pub fn get_closest_node(&self, id: &Identifier) -> Option<Peer> {
+        let bucket_index = self.xor_bucket_index(id);
+
+        for bucket in self.buckets.iter().skip(bucket_index) {
+            if !bucket.map.is_empty() {
+                let k = bucket.map.keys().next().unwrap();
+                let (k, v) = bucket.map.get_key_value(k).unwrap();
+                return Some(Peer {
+                    id: *k,
+                    socket_addr: *v,
+                });
+            }
+        }
+        None
+    }
+
+    // TODO: Create complete routing table logic (return K closest nodes instead of indexed bucket)
+    // pub fn get_closest_nodes() {}
+
     pub fn get_bucket_for(&self, id: &Identifier) -> Option<&HashMap<[u8; 32], SocketAddr>> {
         let bucket_index = self.xor_bucket_index(id);
         if self.buckets[bucket_index].map.is_empty() {
             println!("BUCKET IS EMPTY");
             return None;
         }
-        println!("BUCKET ISN't EMPTY");
+        println!("BUCKET ISN'T EMPTY");
         Some(&self.buckets[bucket_index].map)
     }
 
-    // TODO: pub fn get_closest_nodes(id)
-
+    // TODO: Move to helper.rs  xor(id1, id2)
     pub fn xor_bucket_index(&self, id: &Identifier) -> usize {
         let x = U256::from(self.peer.id);
         let y = U256::from(id);
@@ -73,3 +91,5 @@ impl KbucketTable {
         MAX_BUCKETS - (xor_distance.leading_zeros() as usize)
     }
 }
+
+// TODO: Create tests for bucket logic
