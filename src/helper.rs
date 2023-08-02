0b6760aa1a717025c5e6557bfc96581a3d861f4e
EchoAlice <echoalice0@gmail.com>
2023-08-02 09:03:43 -0600

    Delete commented helper functions

--- src/helper.rs
+++ src/helper.rs
@@ -23,39 +23,3 @@ pub fn xor_bucket_index(x: &Identifier, y: &Identifier) -> usize {
 
     MAX_BUCKETS - (xor_distance.leading_zeros() as usize)
 }
-/*
-// Helper Functions for Tests
-// -------------------------------------------------------------------------
-pub async fn make_nodes(n: u8) -> (Node, Vec<Node>) {
-    let local_node = make_node(0).await;
-    let mut remote_nodes = Vec::new();
-
-    for i in 1..n {
-        remote_nodes.push(make_node(i).await);
-    }
-
-    (local_node, remote_nodes)
-}
-
-pub async fn make_node(index: u8) -> Node {
-    let peer = make_peer(index);
-    Node::new(peer).await
-}
-
-pub fn make_peer(index: u8) -> Peer {
-    let ip = "127.0.0.1".parse::<IpAddr>().unwrap();
-    let port_start = 9000_u16;
-
-    let mut id = [0_u8; 32];
-    id[31] += index;
-    let port = port_start + index as u16;
-
-    let socket_addr = net::SocketAddr::new(ip, port);
-
-    Peer {
-        id,
-        socket_addr: SocketAddr { addr: socket_addr },
-    }
-}
-// -------------------------------------------------------------------------
- */
